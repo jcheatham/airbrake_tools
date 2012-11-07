3e5c2096e54b787cf7eb81009a3d9cffbaf9ea58
Jonathan Cheatham <coaxis@gmail.com>
2012-11-06 19:11:02 -0800

    Basic framework update

--- lib/airbrake_tools.rb
+++ lib/airbrake_tools.rb
@@ -1,4 +1,58 @@
 require "airbrake_tools/version"
 
 module AirbrakeTools
+  class << self
+    def cli(argv)
+      options = extract_options(argv)
+
+      subdomain, key = argv
+      if subdomain.to_s.empty? || key.to_s.empty?
+        puts "Usage instructions: airbrake-tools --help"
+        return 1
+      end
+
+      hot(subdomain, key, options) || 0
+    end
+
+    def hot(subdomain, key, options)
+      puts "Calling hot with #{subdomain}, #{key}, #{options.inspect}"
+      return 0
+    end
+
+    private
+
+    def extract_options(argv)
+      options = {
+      }
+      OptionParser.new do |opts|
+        opts.banner = <<-BANNER.gsub(" "*12, "")
+            Get the hotest airbrake errors
+
+            Usage:
+                airbrake-tools subdomain key [options]
+
+            Options:
+        BANNER
+        opts.on("-h", "--help", "Show this.") { puts opts; exit }
+        opts.on("-v", "--version", "Show Version"){ puts "airbrake-tools #{VERSION}"; exit }
+      end.parse!(argv)
+      options
+    end
+
+    def run(cmd)
+      all = ""
+      puts cmd
+      IO.popen(cmd) do |pipe|
+        while str = pipe.gets
+          all << str
+          puts str
+        end
+      end
+      [$?.success?, all]
+    end
+
+    def run!(command)
+      raise "Command failed #{command}" unless run(command).first
+    end
+  end
 end
--- spec/airbrake_tools_spec.rb
+++ spec/airbrake_tools_spec.rb
@@ -1,7 +1,41 @@
-require "spec_helper"
+ROOT = File.expand_path('../../', __FILE__)
 
-describe AirbrakeTools do
-  it "has a VERSION" do
-    AirbrakeTools::VERSION.should =~ /^[\.\da-z]+$/
+describe "airbrake-tools" do
+  def run(command, options={})
+    result = `#{command} 2>&1`
+    message = (options[:fail] ? "SUCCESS BUT SHOULD FAIL" : "FAIL")
+    raise "[#{message}] #{result} [#{command}]" if $?.success? == !!options[:fail]
+    result
   end
+
+  def airbrake_tools(args, options={})
+    run "#{ROOT}/bin/airbrake-tools #{args}", options
+  end
+
+  before do
+    Dir.chdir ROOT
+  end
+
+  describe "basics" do
+    it "shows its usage without arguments" do
+      airbrake_tools("", :fail => true).should include("Usage")
+    end
+
+    it "shows its usage with -h" do
+      airbrake_tools("-h").should include("Usage")
+    end
+
+    it "shows its usage with --help" do
+      airbrake_tools("--help").should include("Usage")
+    end
+
+    it "shows its version with -v" do
+      airbrake_tools("-v").should =~ /^airbrake-tools \d+\.\d+\.\d+$/
+    end
+
+    it "shows its version with --version" do
+      airbrake_tools("-v").should =~ /^airbrake-tools \d+\.\d+\.\d+$/
+    end
+  end
+
 end
