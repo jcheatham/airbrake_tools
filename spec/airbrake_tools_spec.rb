747bd8ba85757ec521189c9f88e8ac4fc8b46a29
grosser <grosser.michael@gmail.com>
2013-02-23 08:05:01 -0800

    refactor specs a bit

--- spec/airbrake_tools_spec.rb
+++ spec/airbrake_tools_spec.rb
@@ -4,72 +4,72 @@ require 'airbrake_tools'
 ROOT = File.expand_path('../../', __FILE__)
 
 describe "airbrake-tools" do
-  def run(command, options={})
-    result = `#{command} 2>&1`
-    message = (options[:fail] ? "SUCCESS BUT SHOULD FAIL" : "FAIL")
-    raise "[#{message}] #{result} [#{command}]" if $?.success? == !!options[:fail]
-    result
-  end
+  before { Dir.chdir ROOT }
 
-  def airbrake_tools(args, options={})
-    run "#{ROOT}/bin/airbrake-tools #{args}", options
-  end
-
-  let(:config) { YAML.load(File.read("spec/fixtures.yml")) }
-
-  before do
-    Dir.chdir ROOT
-  end
-
-  describe "basics" do
-    it "shows its usage without arguments" do
-      airbrake_tools("", :fail => true).should include("Usage")
+  describe "CLI" do
+    def run(command, options={})
+      result = `#{command} 2>&1`
+      message = (options[:fail] ? "SUCCESS BUT SHOULD FAIL" : "FAIL")
+      raise "[#{message}] #{result} [#{command}]" if $?.success? == !!options[:fail]
+      result
     end
 
-    it "shows its usage with -h" do
-      airbrake_tools("-h").should include("Usage")
+    def airbrake_tools(args, options={})
+      run "#{ROOT}/bin/airbrake-tools #{args}", options
     end
 
-    it "shows its usage with --help" do
-      airbrake_tools("--help").should include("Usage")
+    let(:config) { YAML.load(File.read("spec/fixtures.yml")) }
+
+    describe "basics" do
+      it "shows its usage without arguments" do
+        airbrake_tools("", :fail => true).should include("Usage")
+      end
+
+      it "shows its usage with -h" do
+        airbrake_tools("-h").should include("Usage")
+      end
+
+      it "shows its usage with --help" do
+        airbrake_tools("--help").should include("Usage")
+      end
+
+      it "shows its version with -v" do
+        airbrake_tools("-v").should =~ /^airbrake-tools \d+\.\d+\.\d+$/
+      end
+
+      it "shows its version with --version" do
+        airbrake_tools("-v").should =~ /^airbrake-tools \d+\.\d+\.\d+$/
+      end
     end
 
-    it "shows its version with -v" do
-      airbrake_tools("-v").should =~ /^airbrake-tools \d+\.\d+\.\d+$/
+    describe "hot" do
+      it "kinda works" do
+        output = airbrake_tools("#{config["subdomain"]} #{config["auth_token"]} hot")
+        output.should =~ /#\d+\s+\d+\.\d+\/hour\s+total:\d+/
+      end
     end
 
-    it "shows its version with --version" do
-      airbrake_tools("-v").should =~ /^airbrake-tools \d+\.\d+\.\d+$/
+    describe "list" do
+      it "kinda works" do
+        output = airbrake_tools("#{config["subdomain"]} #{config["auth_token"]} list")
+        output.should include("Page 1 ")
+        output.should =~ /^\d+/
+      end
     end
-  end
 
-  describe "hot" do
-    it "kinda works" do
-      output = airbrake_tools("#{config["subdomain"]} #{config["auth_token"]} hot")
-      output.should =~ /#\d+\s+\d+\.\d+\/hour\s+total:\d+/
+    describe "summary" do
+      it "kinda works" do
+        output = airbrake_tools("#{config["subdomain"]} #{config["auth_token"]} summary 51344729")
+        output.should include("last retrieved notice: ")
+        output.should include("last 2 hours: ")
+      end
     end
-  end
 
-  describe "list" do
-    it "kinda works" do
-      output = airbrake_tools("#{config["subdomain"]} #{config["auth_token"]} list")
-      output.should include("Page 1 ")
-      output.should =~ /^\d+/
-    end
-  end
-
-  describe "summary" do
-    it "kinda works" do
-      output = airbrake_tools("#{config["subdomain"]} #{config["auth_token"]} summary 51344729")
-      output.should include("last retrieved notice: ")
-      output.should include("last 2 hours: ")
-    end
-  end
-
-  describe "newest" do
-    it "kinda works" do
-      output = airbrake_tools("#{config["subdomain"]} #{config["auth_token"]} new")
-      output.should =~ /#\d+\s+\d+\.\d+\/hour\s+total:\d+/
+    describe "new" do
+      it "kinda works" do
+        output = airbrake_tools("#{config["subdomain"]} #{config["auth_token"]} new")
+        output.should =~ /#\d+\s+\d+\.\d+\/hour\s+total:\d+/
+      end
     end
   end
 
